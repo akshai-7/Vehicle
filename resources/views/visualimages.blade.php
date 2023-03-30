ea3f80268ed63d7f6335c3b30d0315b1ffb0596c
Your Namekshai-7 <akshai2537@gmail.com>
2023-03-30 12:06:06 +0530

    first commit

--- resources/views/visualimages.blade.php
+++ resources/views/visualimages.blade.php
@@ -8,10 +8,10 @@
                         <h4 style="color:#bf0e3a;margin-left:560px"> <i class="fa-sharp fa-regular fa-circle-xmark"></i></h4>
                     </a>
                 </div>
-                <div>
+                <div id="popUpimgae">
                     @foreach (explode(',', $visual->image) as $image)
-                        <img src="{{ url('images/' . $image) }}" class="rounded-0 border border-secondary" width="150px"
-                            height="120px">
+                        <img src="{{ url('images/' . $image) }}" class="rounded-0 border border-secondary" width="170px"
+                            height="160px">
                         <span></span>
                     @endforeach
                 </div>
