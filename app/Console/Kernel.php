a1f38ade60f35b4752029a2d2af44dca9abab164
Your Namekshai-7 <akshai2537@gmail.com>
2023-03-30 22:00:19 +0530

    first commit

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,8 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('weekly:mail')->everyMinute()->timezone('UCT');
-        // $schedule->command('weekly:mail')->weekly()->fridays()->at('8:00')->timezone('UCT');
-
+        // $schedule->command('weekly:mail')->weekly()->fridays()->at('8:00')->timezone('Asia/Kolkata');
     }
 
     /**
--- resources/views/mail/sendmail.blade.php
+++ resources/views/mail/sendmail.blade.php
@@ -1,1 +1,2 @@
-M&d Foundations
+<h3>M&d Foundations</h3><br>
+<h5>Today your Vehicle Inspection day</h5>
